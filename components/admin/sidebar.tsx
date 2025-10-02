163c288c4021b89e7aa6601c7e4aee41f264b4a6
Drenzzz <realdrenzzz@gmail.com>
2025-10-02 18:08:46 +0700

    style(theme): update color palette for theme

--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -56,7 +56,7 @@ export function Sidebar({ isCollapsed }: SidebarProps) {
                 className={cn(
                   'flex h-10 w-10 w-full items-center justify-center rounded-lg transition-colors hover:text-foreground',
                   isActive
-                    ? 'bg-primary text-primary-foreground'
+                    ? 'bg-sidebar-primary text-sidebar-primary-foreground'
                     : 'text-muted-foreground'
                 )}
               >
@@ -75,7 +75,7 @@ export function Sidebar({ isCollapsed }: SidebarProps) {
         className={cn(
           'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary',
           isActive
-            ? 'bg-muted text-primary font-semibold'
+            ? 'bg-sidebar-primary text-sidebar-primary-foreground font-semibold '
             : 'text-muted-foreground'
         )}
       >
