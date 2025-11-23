154ee3fd199c3184591d6cd9024012082e80d0bb
Drenzzz <realdrenzzz@gmail.com>
2025-11-23 19:35:04 +0700

    Revert "chore: upgrade prisma to v7 and update datasource configuration"
    This reverts commit 9f0c2b3918a822bbd0faa03c22cec45a160efff3.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
+const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   global.prisma = prisma;
 }
 
